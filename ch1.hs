6668f555d30093f7c5e8125388fb15579efd572c
zhangjiji <programmer.zhangjiji@gmail.com>
2014-02-06 01:23:28 +0800

    ex3 word count

--- ch1.hs
+++ ch1.hs
@@ -1,5 +1,7 @@
 -- file: ch01/WC.hs
 
-main = interact wordCount
-  where wordCount input = show (length (lines input)) ++ "\n"
+-- main = interact wordCount
+--   where wordCount input = show (length (lines input)) ++ "\n"
 
+main = interact wordCount
+  where wordCount input = show (length (words input)) ++ "\n"
