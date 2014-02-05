1f377b143d5a323b2fe985e1de7bbb01b1c80af8
zhangjiji <programmer.zhangjiji@gmail.com>
2014-02-06 01:25:45 +0800

    all reserved

--- ch1.hs
+++ ch1.hs
@@ -3,5 +3,8 @@
 -- main = interact wordCount
 --   where wordCount input = show (length (lines input)) ++ "\n"
 
+-- main = interact wordCount
+--   where wordCount input = show (length (words input)) ++ "\n"
+
 main = interact wordCount
   where wordCount input = show (length input) ++ "\n"
