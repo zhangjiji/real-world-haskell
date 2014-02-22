abbf661195e95b3604738993fb01965ddb87b470
zhangjiji <programmer.zhangjiji@gmail.com>
2014-02-22 23:59:00 +0800

    random

--- ch14/Random.hs
+++ ch14/Random.hs
@@ -32,3 +32,36 @@ getRandom' = do
   let (val, gen') = random gen
   put gen'
   return val
+
+runTwoRandoms :: IO (Int, Int)
+runTwoRandoms = do
+  oldState <- getStdGen
+  let (result, newState) = runState getTwoRandoms oldState
+  setStdGen newState
+  return result
+
+data CountedRandom = CountedRandom {
+  crGen :: StdGen
+  , crCount :: Int
+  }
+
+type CRState = State CountedRandom
+
+getCountedRandom :: Random a => CRState a
+getCountedRandom = do
+  st <- get
+  let (val, gen') = random (crGen st)
+  put CountedRandom { crGen = gen', crCount = (crCount st) + 1 }
+  return val
+
+getCount :: CRState Int
+getCount = crCount `liftM` get
+
+putCount :: Int -> CRState ()
+putCount a = do
+  st <- get
+  put st { crCount = a }
+
+putCountModify :: Int -> CRState ()
+putCountModify a = modify $ \st -> st { crCount = a }
+
