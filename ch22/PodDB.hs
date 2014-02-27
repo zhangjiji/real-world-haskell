03060c22684bfcb535b8f0c0dbe8f733eb269349
zhangjiji <programmer.zhangjiji@gmail.com>
2014-02-27 16:14:59 +0800

    add entry into db

--- ch22/PodDB.hs
+++ ch22/PodDB.hs
@@ -12,8 +12,27 @@ connect fp =
      prepDB dbh
      return dbh
 
--- prepDB :: IConnection conn => conn -> IO ()
--- prepDB dbh =
---   do tables <- getTables dbh
---      when (not ("podcasts" `elem` tables)) $
---        do run
+prepDB :: IConnection conn => conn -> IO ()
+prepDB dbh =
+  do tables <- getTables dbh
+     when (not ("podcasts" `elem` tables)) $
+       do run dbh "CREATE TABLE podcasts (castid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, castURL TEXT NOT NULL UNIQUE)" []
+          return ()
+     when (not ("episodes" `elem` tables)) $
+       do run dbh "CREATE TABLE episodes (epid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, epcastid INTEGER NOT NULL, epurl TEXT NOT NULL, epdone INTEGER NOT NULL, UNIQUE(epcastid, epurl), UNIQUE(epcastid, epid))" []
+          return ()
+     commit dbh
+
+addPodcast :: IConnection conn => conn -> Podcast -> IO Podcast
+addPodcast dbh podcast =
+  handleSql errorHandler $
+    do
+      run dbh "INSERT INTO podcasts (castURL) VALUES (?)" [toSql (castURL podcast)]
+      r <- quickQuery' dbh "SELECT castid FROM podcasts WHERE castURL = ?" [toSql (castURL podcast)]
+--      commit dbh
+      
+      case r of
+        [[x]] -> return $ podcast {castId = fromSql x}
+        y -> fail $ "Select None"
+  where errorHandler e =
+          do fail $ "error"
